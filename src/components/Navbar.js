388cb6122578d7bce27e1d03d50f4623f9e83535
Monet Thigpen <monetthigpen@Monets-MacBook-Pro.local>
2022-03-09 21:00:08 -0500

    made nav collapse

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,14 @@ function NavbarM() {
         <Navbar id="nvBck">
             <Container>
             <Navbar.Brand id="home" href="#home">Monet Thigpen</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link id="aboutN" href="#about">About</Nav.Link>
-            <Nav.Link id="projectsN" href="#projects">Projects</Nav.Link>
-            <Nav.Link id="skillsN" href="#skills">Skills</Nav.Link>
-            <Nav.Link id="contactN" href="#contact">Contact</Nav.Link>
-            </Nav>
+                <Navbar.Collapse>
+                    <Nav className="me-auto">
+                        <Nav.Link id="aboutN" href="#about">About</Nav.Link>
+                        <Nav.Link id="projectsN" href="#projects">Projects</Nav.Link>
+                        <Nav.Link id="skillsN" href="#skills">Skills</Nav.Link>
+                        <Nav.Link id="contactN" href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
