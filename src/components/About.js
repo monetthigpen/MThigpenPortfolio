dbfc26410ce80cf8189d91c86ed22704bfbed6b4
Monet Thigpen <monetthigpen@Monets-MacBook-Pro.local>
2022-03-10 00:10:16 -0500

    verbage

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,8 @@ function About() {
                     <br/>
                     <h5  id="aboutTxt" className="ptSans"> Hi, I'm Monet.
                     <br/>
-                    I love to enhance small and large organization's web presence.</h5>
-                    <p className="abtSmTxt"> I am a Full Stack Web Developer trained at the University of Pennsylvania with a passion for all things Web. I know that now and in the future all successful businesses will need to be readily accessible online, so I want to create and innovate what businesses can do to improve their web presence. Currently, I am helping my organization enhance internal document navigation, management, and creation. With a clean and efficient UX and UI the internal processes are improved and production is increased., benefitting the company and the clients
+                    I love to enhance the web presence of small and large organizations.</h5>
+                    <p className="abtSmTxt"> I am a Full Stack Web Developer trained at the University of Pennsylvania with a passion for all things Web. I know that now and in the future all successful businesses will need to be readily accessible online, so I want to create and innovate what businesses can do to improve their web presence. Currently, I am helping my organization enhance internal document navigation, management, and creation. With a clean and efficient UX and UI the internal processes are improved and production is increased., benefitting the company and the clients.
                     </p>
                     <Row>
                         <Col sm={6}>
