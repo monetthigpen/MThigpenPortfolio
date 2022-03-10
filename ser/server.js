9ff3ccc26c09c9c99b490433c45a8f936c5d96dd
Monet Thigpen <monetthigpen@Monets-MacBook-Pro.local>
2022-03-09 23:32:32 -0500

    manifest json update

--- ser/server.js
+++ ser/server.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 
