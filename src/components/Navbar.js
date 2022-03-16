90a47bd9d08b5924013d0115c818d7516177ebae
Monet Thigpen <monetthigpen@Monets-MacBook-Pro.local>
2022-03-15 21:46:16 -0400

    sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,10 +10,10 @@ function NavbarM() {
             <Navbar.Brand id="home" href="#home">Monet Thigpen</Navbar.Brand>
                 <Navbar.Collapse>
                     <Nav className="me-auto">
-                        <Nav.Link id="aboutN" href="#about">About</Nav.Link>
-                        <Nav.Link id="projectsN" href="#projects">Projects</Nav.Link>
-                        <Nav.Link id="skillsN" href="#skills">Skills</Nav.Link>
-                        <Nav.Link id="contactN" href="#contact">Contact</Nav.Link>
+                        <Nav.Link className="navText" id="aboutN" href="#about">About</Nav.Link>
+                        <Nav.Link className="navText" id="projectsN" href="#projects">Projects</Nav.Link>
+                        <Nav.Link className="navText" id="skillsN" href="#skills">Skills</Nav.Link>
+                        <Nav.Link className="navText" id="contactN" href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
